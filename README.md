1dfae8b2c9976ccd61757bf693f3c61dddd75f9d
James_Dang <dangja@oregonstate.edu>
2022-08-19 23:53:48 -0700

    Added Merge Sort Visualization

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # Sorting-Time-Visualizer
-Using Pyplot, displays and compares the sorting time between different sorting algorithms
+Using Matplotlib and timer as the decorator, displays and compares the sorting time between different sorting algorithms
 
-The main code is within the sort_timer.py file, with the rest of the .py files as imported dependencies. When run, sort_timer.py will display - via Pyplot - a plotting chart comparing the sorting time between Bubble sort and Insertion sort. It will also display a plotting chart that shows the time it takes to find the mode within a data set is O(n). 
+The main code is within the sort_timer.py file, with the rest of the .py files as imported dependencies. When run, sort_timer.py will display - via Pyplot - a plotting chart comparing the sorting time between Bubble sort and Insertion sort.
 
 The time is measured using a simple sort_timer function that takes the difference between the time when the sorting functions start and end, with the sort_timer function used as decorator that wraps the sorting functions.
 
+Currently, this project supports the following sorting algorithms: Bubble Sort, Insertion Sort, Quicksort, and Merge Sort.
+
 If you would like to compare using your own data sets or with different sorting algorithms, simply clone the repository and insert your data sets / algorithms into the sort_timer.py file.
+
+
+**To Get Started**
+
+Clone the repository to your directory. If you're using unix or Windows command line, simply navigate to your directory as follow:
+```
+cd <repository path>
+```
+Then, run the following command:
+```
+python3 sort_timer.py
+```
+
+Alternatively, you can use whichever editor (e.g. PyCharm, VSCode, etc....) to compile the sort_timer.py file instead. Happy coding!
--- sort_timer.py
+++ sort_timer.py
@@ -27,7 +27,7 @@ def bubble_sort(array):
     """
     Sort a list in ascending order using Bubble Sort
     :param array: a list of integers
-    :return: a sorted list in ascending order
+    :return: None
     """
     for pass_num in range(len(array) - 1):
         for index in range(len(array) - 1 - pass_num):
@@ -42,7 +42,7 @@ def insertion_sort(array):
     """
     Sort a list in ascending order using Insertion Sort
     :param array: a list of integers
-    :return: a sorted list in ascending order
+    :return: None
     """
     for index in range(1, len(array)):
         value = array[index]
@@ -60,7 +60,7 @@ def quick_sort(array, start, end):
     :param array: A list of integers
     :param start: starting index
     :param end: ending index
-    :return: a sorted list in ascending order
+    :return: None
     """
 
     if start >= end:
@@ -89,43 +89,89 @@ def quick_sort(array, start, end):
     quick_sort(array, partition_index + 1, end)
 
 
+@sort_timer
+def merge_sort(arr):
+    """
+    Sorts an array using Merge Sort
+    :param arr: list of integers
+    :return: None
+    """
 
-def compare_sort_time(func1=bubble_sort, func2=insertion_sort, func3=quick_sort):
+    if len(arr) > 1:
+        # Finding the mid of the array
+        mid = len(arr) // 2
+        # Dividing the array elements
+        L = arr[:mid]
+        # into 2 halves
+        R = arr[mid:]
+        # Sorting the first half
+        merge_sort(L)
+        # Sorting the second half
+        merge_sort(R)
+
+        i = j = k = 0
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+def compare_sort_time(func1=bubble_sort, func2=insertion_sort, func3=quick_sort, func4=merge_sort):
     """
-    compares the sorting time between two sorting functions for the same list of numbers
-    ranging between 1000 to 10000 integers. The integers' range is 1 <= int <= 10000
-    :param func1: bubble sort function
-    :param func2: insertion sort function
-    :return: a graph that compares func1 and func2 sorting time as the y-axis and the data sizes
-    as the x-axis
+    Compares the sorting time between all 4 different sorting algorithms over a sample size of 1000 to 20000
+    randomly generated integers. The sorting times are plotted and displayed for each algorithm
+    :param func1: Bubble Sort
+    :param func2: Insertion Sort
+    :param func3: Quick Sort
+    :param func4: Merge Sort
+    :return: A graph showing the sorting time differences between all 4 algorithms
     """
-    # create two lists that hold the sorting time for bubble sort vs insertion sort
+    # create lists to hold sorting time for each algorithm
     bubble_sort_time = []
     insertion_sort_time = []
     quick_sort_time = []
+    merge_sort_time = []
     int_list_size = [num*1000 for num in range(1, 21)]  # holds the lists' size from 1000 to 20000
 
-    # generate two identical lists of integers between range [1, 10000]
+    # generate four identical lists of integers between range [1, 10000]
     for num in range(1, 21):
         # create a list of size equal to num * 1000
         list_1 = [random.randrange(1, 10001) for size in range(num*1000)]
         list_2 = list(list_1)
         list_3 = list(list_1)
+        list_4 = list(list_1)
 
-        # call decorated bubble_sort and insertion_sort
-        # to get sorting time and append time to each function's sort_time list
+        # call decorated sorting functions to get sorting time. Append time to each function's sort_time list
         bubble_sort_time.append(func1(list_1))
         insertion_sort_time.append(func2(list_2))
-        quick_sort_time.append(func3(list_3, start=0, end=len(list_3)-1))
+        quick_sort_time.append(func3(list_3, start=0, end=(len(list_3)-1)))
+        merge_sort_time.append(func4(list_4))
 
     # plot bubble sort's respective sorting times to the integers' lists size
-    # x-axis = number of integers sorted, from 1000 to 10000
+    # x-axis = number of integers sorted, from 1000 to 20000
     # y-axis = sorting time in respect to integers' lists size
     pyplot.plot(int_list_size, bubble_sort_time, color='red', marker='o', linestyle='dashed', linewidth=2, label='Bubble Sort')
     # plot insertion sort's respective sorting times to the integers' lists size
     pyplot.plot(int_list_size, insertion_sort_time, color='green', marker='o', linestyle='dashed', linewidth=2, label='Insertion Sort')
     # plot quick sort's respective sorting times to the integers' lists size
     pyplot.plot(int_list_size, quick_sort_time, color='yellow', marker='o', linestyle='dashed', linewidth=2, label='Quick Sort')
+    # plot merge sort's respective sorting times to the integers' lists size
+    pyplot.plot(int_list_size, merge_sort_time, color='purple', marker='o', linestyle='dashed', linewidth=2, label='Merge Sort')
+
     pyplot.xlabel("Number of Integers Sorted")
     pyplot.ylabel("Sorting Time(seconds)")
     pyplot.legend(loc='upper left')
@@ -134,4 +180,4 @@ def compare_sort_time(func1=bubble_sort, func2=insertion_sort, func3=quick_sort)
 
 
 if __name__ == '__main__':
-    compare_sort_time(func1=bubble_sort, func2=insertion_sort, func3=quick_sort)
+    compare_sort_time(func1=bubble_sort, func2=insertion_sort, func3=quick_sort, func4=merge_sort)
